9294aee641e0d4fd209720ef669567166b93f838
jellalis <ppanag94@gmail.com>
2026-06-08 23:51:03 +0300

    test: add test_get_user_success and test_update_user

--- test/test_auth.py
+++ test/test_auth.py
@@ -1,6 +1,6 @@
 import pytest
 from fastapi import HTTPException
-from auth.crud import create_user,get_user_or_404,delete_user
+from auth.crud import create_user,get_user_or_404,delete_user,update_user
 from auth.schemas import UserCreate
 from auth.hashing import ver_pass
 
@@ -49,3 +49,35 @@ def test_delete_user(db):
     with pytest.raises(HTTPException) as exc_info:
         get_user_or_404(db,user_id=created.id)
     assert exc_info.value.status_code==404
+
+def test_get_user_success(db):
+    user_data=UserCreate(
+    name="Panos",
+    email="Panos@gmail.com",
+    password="Panos"
+        
+    )
+    created=create_user(db,user_data)
+    get_user=get_user_or_404(db,created.id)
+    assert get_user.id==created.id
+    assert get_user.name==created.name
+    assert get_user.email==created.email
+    assert ver_pass("Panos",created.hashed_password)
+
+def test_update_user(db):
+    user_data=UserCreate(
+    name="Panos",
+    email="Panos@gmail.com",
+    password="Panos"        
+    )
+    created=create_user(db,user_data)
+    new_user_data=UserCreate(
+    name="kalos",
+    email="Panosss@gmail.com",
+    password="Panos"        
+    )
+    updated_user=update_user(db,created.id,new_user_data)
+    assert created.id==updated_user.id
+    assert new_user_data.name==updated_user.name
+    assert new_user_data.email==updated_user.email
+    assert ver_pass("Panos",updated_user.hashed_password)
