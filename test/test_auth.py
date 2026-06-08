8769200eb4ec123a809ab85d52edc29d74146558
jellalis <ppanag94@gmail.com>
2026-06-08 22:20:24 +0300

    chore: rename test_crud.py to test_auth.py

--- test/test_auth.py
+++ test/test_auth.py
@@ -1,9 +1,9 @@
-from auth.crud import create_user
-from auth.schemas import UserCreate
-from auth.hashing import ver_pass
 import pytest
 from fastapi import HTTPException
-from auth.crud import get_user_or_404,delete_user
+from auth.crud import create_user,get_user_or_404,delete_user
+from auth.schemas import UserCreate
+from auth.hashing import ver_pass
+
 def test_create_user(db):
     
     user_data=UserCreate(
@@ -17,7 +17,19 @@ def test_create_user(db):
     assert created.id is not None
     assert created.email=="Panos@gmail.com"
     assert ver_pass("Panos",created.hashed_password)
+def test_create_user_duplicate_email(db):
+    user_data=UserCreate(
+        name="Panos",
+        email="Panos@gmail.com",
+        password="Panos"
+        
+    )
     
+    created=create_user(db,user_data)
+    with pytest.raises(HTTPException) as exc_info:
+        created=create_user(db,user_data)
+    assert exc_info.value.status_code==409
+
 
 
 def test_get_user_not_found(db):
