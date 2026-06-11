a1639229de547f687c26afb253dbfb20af7ecd4f
jellalis <ppanag94@gmail.com>
2026-06-12 01:54:45 +0300

    test: add client and auth_headers fixtures and category integration tests

--- test/conftest.py
+++ test/conftest.py
@@ -7,7 +7,12 @@ from auth.models import User
 from budget.models import Budget
 from categories.models import Category
 from transactions.models import Transaction
-
+from main import app
+from db.database import get_db
+from starlette.testclient import TestClient
+from auth.crud import create_user
+from auth.jwt import create_tok
+from auth.schemas import UserCreate
 TEST_DATABASE_URL = "sqlite://"
 
 engine= create_engine(
@@ -29,4 +34,21 @@ def db():
     finally:
         session.close() 
         Base.metadata.drop_all(bind=engine)
-        
+
+@pytest.fixture
+def client(db):
+    app.dependency_overrides[get_db]=lambda:db
+    yield TestClient(app)
+    app.dependency_overrides.clear()
+
+
+@pytest.fixture
+def auth_headers(db):
+    user_data=UserCreate(
+    name="Panos",
+    email="Panos@gmail.com",
+    password="Panos"        
+    )
+    created=create_user(db,user_data)
+    created_token=create_tok(created.id)
+    return {"Authorization": f"Bearer {created_token}"}
